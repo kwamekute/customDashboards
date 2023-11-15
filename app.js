38092c9e65c57f5c4eaa501df7446f5103f37739
kwamekute <thatkutekwame@gmail.com>
2023-11-15 21:55:23 +0000

    added images folder

---
+++ app.js
@@ -0,0 +1,10 @@
+const sidebar = document.querySelector(".menu-container");
+const body = document.querySelector('body');
+
+sidebar.addEventListener('mouseover', () => {
+    body.style.overflow = 'hidden';
+})
+
+sidebar.addEventListener("mouseout", () => {
+    body.style.overflow = 'auto';
+})
