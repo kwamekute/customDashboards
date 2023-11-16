c0131c5f1cbe95ef04dc993a5fad85e69e82932b
kwamekute <thatkutekwame@gmail.com>
2023-11-16 00:01:01 +0000

    sidebar in-progress

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 const sidebar = document.querySelector(".menu-container");
 const body = document.querySelector('body');
 
-sidebar.addEventListener('mouseover', () => {
-    body.style.overflow = 'hidden';
-})
+// sidebar.addEventListener('mouseover', () => {
+//     body.style.overflow = 'hidden';
+// })
 
-sidebar.addEventListener("mouseout", () => {
-    body.style.overflow = 'auto';
-})
+// sidebar.addEventListener("mouseout", () => {
+//     body.style.overflow = 'auto';
+// })
